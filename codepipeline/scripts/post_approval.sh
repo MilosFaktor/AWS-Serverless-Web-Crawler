356905cc90917aac509c3fc0b5038ad30dc849d4
M1oir <faktor.milos@gmail.com>
2025-08-08 21:23:29 +0200

    Update post_approval.sh

--- codepipeline/scripts/post_approval.sh
+++ codepipeline/scripts/post_approval.sh
@@ -6,5 +6,5 @@ curl -sS -X POST \
 -H "Authorization: token $GITHUB_TOKEN" \
 -H "Accept: application/vnd.github.v3+json" \
 "https://api.github.com/repos/MilosFaktor/aws-hands-on-lab-SAA/statuses/$CODEBUILD_RESOLVED_SOURCE_VERSION" \
--d "{"state":"success","description":"Pipeline successfully tested and manually approved. ","context":"post-approval-gate"}"
+-d '{"state":"success","description":"Pipeline successfully tested and manually approved. ","context":"post-approval-gate"}'
 
